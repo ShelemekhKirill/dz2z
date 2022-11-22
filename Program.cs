af33a2247c52ef0f018807f60b12e31c2944a559
Kirill Shelemekh <kirill.shelemeh@gmail.com>
2022-11-22 15:36:30 +0300

    2

--- Program.cs
+++ Program.cs
@@ -12,17 +12,65 @@ else{
     Console.WriteLine($"Вторая цифра трехзначного числа: {Numbers[1]}");
 } */
 
+
+
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 // что третьей цифры нет. Берем числа до 100000.
 
-int n = new Random().Next(1, 100000);
-int s = ( n % 1000)/100;
-Console.WriteLine(n);
-if ( s > 0) 
-{
-    Console.WriteLine($"Третья цифра заданного числа: {s}");
+/* Console.WriteLine("Введи число: ");
+string Numbers = Console.ReadLine()!;
+
+if(Numbers.Length > 5){
+    Console.WriteLine("Введено неверное число!");
 }
-else
-{
-    Console.WriteLine("У заданного числа нет третьей цифры");
+else if(Numbers.Length < 3){
+    Console.WriteLine("У заданного числа нет третьей цифры!");
 }
+
+else{
+    Console.WriteLine($"Третья цифра заданного числа: {Numbers[2]}");
+} */
+
+
+
+/* // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7.
+
+Console.WriteLine("введите цифру от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+switch (number)
+{
+    case 1:{
+        Console.WriteLine("будний день: ");
+        break;
+    }
+    case 2:{
+        Console.WriteLine("будний день: ");
+        break;
+    }
+    case 3:{
+        Console.WriteLine("будний день: ");
+        break;
+    }
+    case 4:{
+        Console.WriteLine("будний день: ");
+        break;
+    }
+    case 5:{
+        Console.WriteLine("будний день: ");
+        break;
+    }
+    case 6:{
+        Console.WriteLine("выходной день: ");
+        break;
+    }
+    case 7:{
+        Console.WriteLine("выходной день: ");
+        break;
+    }
+    default:{
+        Console.WriteLine("неправильная цифра: ");  
+        break;
+    }
+} */
